7492a20399a6dfeefb477d66106bce36b3cc48f1
Filip <filip.sucholbiak@gmail.com>
2022-11-08 19:20:49 +0100

    Added new classes

--- src/test/java/pl/kurs/equationsolver/services/SolvingServiceTest.java
+++ src/test/java/pl/kurs/equationsolver/services/SolvingServiceTest.java
@@ -8,18 +8,18 @@ import pl.kurs.equationsolver.exceptions.UnknownOperatorException;
 
 import java.math.BigDecimal;
 
+
 public class SolvingServiceTest {
 
 
     @Test
     public void evaluateExpressionTest() throws InvalidEquationFormatException, UnknownOperatorException {
 
-        BigDecimal result = BigDecimal.valueOf(2 + 2 * 2);
 
         SolvingService solvingService = new SolvingService(new SolvingEventService(new SolvingEventDao()));
         BigDecimal evaluateExpression = solvingService.evaluateExpression("2 + 2 * 2");
 
-        Assert.assertEquals(result, evaluateExpression);
+        Assert.assertEquals(evaluateExpression.doubleValue(), 6.0, 0);
 
     }
 }
